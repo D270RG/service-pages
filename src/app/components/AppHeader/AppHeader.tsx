a1c2cbbfe76ca0668dbcae9434a774f6e85c6879
D270RG <d270rg@gmail.com>
2023-05-07 00:12:39 +0300

    layout fix

--- src/app/components/AppHeader/AppHeader.tsx
+++ src/app/components/AppHeader/AppHeader.tsx
@@ -4,7 +4,7 @@ import './AppHeader.scss';
 
 function AppHeader() {
 	return (
-		<div className='appHeaderContainer px-3 py-2'>
+		<div className='appHeaderContainer px-3'>
 			ЯСветлый
 			<Button className='btn btn-dark'>
 				<i className='bi bi-cart-fill'></i>
